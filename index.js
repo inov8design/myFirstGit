721ca6f475d307ffcf6c0ee6804da2d0e6484178
Allan Browning <allan@inov8design.net>
2019-06-12 14:19:34 +1000

    second function

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ function addnums(numa,numb){
   return result;
 
 }
+function addnums2(numa,numb,numc){
+  var result = numa + numb + numc;
+  return result;
+
+}
 
 console.log("Addtion of two nums = " + addnums(10,5));
+console.log("Addtion of three nums = " + addnums2(10,5,3));
 Console.log("Project 2 extend the string in project 1");
 
 
